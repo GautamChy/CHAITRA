75a056c0f57a9ffaa90c71a1c151058ca69d9a06
MacLap.IN <maclap.in@Gautam.local>
2025-04-07 02:09:10 +0530

    Added

---
+++ DAY_7/for.py
@@ -0,0 +1,21 @@
+
+# iterable - group datatypes or sequential datatype
+# iteration - applying loop from start to end of the sequeantial 
+# iterator - variable used to apply iteration in iterable
+#a = [1,2,3,4,'a','b','c']  # [ ] - iterable ,1,2,3,4,'a','b','c', - iterator - a 
+#for i in a :
+   # print("hello")
+   # print(i)
+   
+a = [1,2,3,4,'a','b','c'] 
+for i in a :  # i vanney ko variable ho,  i ley sabb euta euta word check garchha,eg- i = T,i = h,i = e,i = ......
+        print("Hello")
+        print(i)
+   
+         
+#a = "The light blinded him. but the light shining in his eyes proved him wrong. It came from about 100 feet away and was shining so directly into his eyes he couldn't make out anything about the person holding the light."
+#for i in a: # i vanney ko variable ho,  i ley sabb euta euta word check garchha,eg- i = T,i = h,i = e,i = ......
+    #if i  == 'o' :
+        #print("o found")
+    #else:
+        #print("Not found")
